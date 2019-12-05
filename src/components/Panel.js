a72c05cc0562c844e85ee926829101b541550584
Nick Hoszko <nickhoszko@Nicks-MacBook-Pro.local>
2019-12-04 18:31:54 -0800

    Toggling sections

---
+++ src/components/Panel.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Dashboard from "components/Dashboard"
+
+class Panel extends Component {
+  render() {
+    const { label, value, onSelect } = this.props;
+
+    return (
+      <section
+        className="dashboard__panel"
+        onClick={onSelect}
+      >
+        <h1 className="dashboard__panel-header">{label}</h1>
+        <p className="dashboard__panel-value">{value}</p>
+      </section>
+    );
+  }
+}
+
+export default Panel;
